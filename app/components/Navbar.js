f7ea41cd8c6923024facf56d245cd4a979982084
Howard Kier <howard@Howards-MacBook-Air.local>
2017-12-18 14:45:10 -0600

    saving

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -9,17 +9,17 @@ const Navbar = (props) => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand">Pill Planner   </a>
+                <a className="navbar-brand">Pill Planner</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-item nav-link" href="/campus">Campuses <span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link" href="/students">Students</a>
-                        <a className="nav-item nav-link disabled">Classes</a>
-                        <a className="nav-item nav-link disabled">Schedules</a>
+                        <a className="nav-item nav-link" href="/medicine">Add Medicine <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link" href="/schedule">Schedule</a>
+                        <a className="nav-item nav-link disabled">Doctors</a>
+                        <a className="nav-item nav-link disabled">Place Holder</a>
                     </div>
                 </div>
             </nav>
